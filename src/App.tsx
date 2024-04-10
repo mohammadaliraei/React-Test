7ef21721fe84f0f82a8266ddde18a150164078ab
mohammadaliraei <mobham.des@gmail.com>
2024-04-10 10:34:05 +0330

    update 0.0.1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex w-full h-screen bg-black justify-center items-center">
+      <h1 className="text-4xl text-white uppercase font-bold">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
